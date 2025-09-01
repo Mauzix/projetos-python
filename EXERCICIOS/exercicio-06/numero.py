110e5b401090e2af0e63ff808ac5ac67a26f1ef9
Maurício-silva <116690555+Mauzix@users.noreply.github.com>
2025-09-01 20:02:05 -0300

    mandando mais arq

--- EXERCICIOS/exercicio-06/numero.py
+++ EXERCICIOS/exercicio-06/numero.py
@@ -1,9 +1,11 @@
-n1 = int (input ("digite numero 1:"))
-n2 = int (input ("digite numero 2:"))
-n3 = int (input ("digite numero 3:"))
-if n1 > n2 and n3:
-    print (f" o {n1} é maior")
-elif n2 > n1 and  n2 > n3 :
-    print (f" o {n2} e maior")
-else:
-    print(f"o {n3 } e maior")
+numero1=int (input ("digite o primeiro numero")) 
+numero2=int (input ("digite o segundo numero")) 
+numero3=int (input ("digite o terceiro numero"))
+if numero1 > numero2 :
+    print(f"o numero {numero1} é o maior")
+elif numero2 > numero1 and numero2  > numero3 :
+     print(f"o numero {numero2} é o maior")
+else :
+     print(f"o numero {numero3} é o maior")
+     
+     
